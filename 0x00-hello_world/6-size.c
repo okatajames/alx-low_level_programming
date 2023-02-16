394c4290553fc5714d85609502a4bb774f4cc435
okatajames <jamesokata3@gmail.com>
2023-02-16 18:50:01 +0300

    Initial commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(void)
+{
+    int p;
+    long int q;
+    long long int r;
+    char s;
+    float t;
+
+    printf("The size of char: %lu byte(s)\n", (unsigned long)sizeof(s));
+    printf("The size of an int: %lu byte(s)\n", (unsigned long)sizeof(p));
+    printf("The size of a long int: %lu byte(s)\n", (unsigned long)sizeof(q));
+    printf("The size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(r));
+    printf("The size of a float: %lu byte(s)\n", (unsigned long)sizeof(t));
+
+    return(0);
+
+}
+
