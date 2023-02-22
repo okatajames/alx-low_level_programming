ed54963a5f48b849e53bc343e605167a573103a7
okatajames <jamesokata3@gmail.com>
2023-02-22 13:44:05 +0300

    Initial commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,11 @@
 
 #include <stdio.h>
 
+/**
+* main - storing our function prototypes
+* Return: 0
+*/
+
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
