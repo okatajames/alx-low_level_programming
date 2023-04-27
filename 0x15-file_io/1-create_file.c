fbfb7df7d521f78023df9fe875817fb9bc347d51
JamesOKata <jamesokata3@gmail.com>
2023-04-27 13:59:55 +0300

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,20 +4,27 @@
  * @filename: is the name of the file to create
  * Return: 1 on success, -1 on failure
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int o, w, length = 0;
+
 	if (filename == NULL)
 		return (-1);
+
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (length = 0; text_content[length];)
+			length++;
 	}
+
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
+	w = write(o, text_content, length);
+
 	if (o == -1 || w == -1)
 		return (-1);
+
 	close(o);
+
 	return (1);
 }
--- win_auto.cmd
+++ win_auto.cmd
@@ -2,6 +2,6 @@
 
 git add .
 
-git commit -m "Initial Commit"
+git commit -m "0x15-file_io"
 
 git push
