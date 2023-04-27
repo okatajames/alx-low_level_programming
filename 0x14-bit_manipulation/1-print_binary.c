a8deb6ccaf3c2e705b7e9095d53fd81fe1d2f08a
JamesOKata <jamesokata3@gmail.com>
2023-04-27 15:29:50 +0300

    0x14-bit_manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,38 +3,46 @@
  * _power - calculate (base and power)
  * @base: base of the exponet
  * @pow: power of the exponet
+ *
  * Return: value of base and power
  */
+
 unsigned long int _power(unsigned int base, unsigned int pow)
 {
-	unsigned long int num =1;
+	unsigned long int integ = 1;
 	unsigned int z;
 
 	for (z = 1; z <= pow; z++)
-		num *= base;
-	return (num);
+		{
+		integ *= base;
+		}
+
+	return (integ);
 }
+
 /**
  * print_binary - prints the binary representation of a number
- * @n: num of prented
+ * @n: integ of prented
+ *
  * Return: void
  */
+
 void print_binary(unsigned long int n)
 {
 	unsigned long int dev, result;
-	char flag = 0;
-	dev = _power(2, sizeof(unsigned long int) * 8 - 1);
+	char ident = 0;
 
+	dev = _power(2, sizeof(unsigned long int) * 8 - 1);
 	while (dev != 0)
 	{
 		result = n & dev;
 		if (result == dev)
 		{
-			flag = 1;
+			ident = 1;
 			_putchar('1');
 
 		}
-		else if (flag == 1 || dev == 1)
+		else if (ident == 1 || dev == 1)
 		{
 			_putchar('0');
 		}
--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,6 +4,7 @@
  * get_bit - returns the value of a bit at a given index.
  * @n: checking bits
  * @index: which to check bit
+ *
  * Return: the value of the bit at index
  */
 int get_bit(unsigned long int n, unsigned int index)
