7b6ac7c75c80df581199a1b4731b80708ff8f79e
JamesOKata <jamesokata3@gmail.com>
2023-05-04 14:19:11 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,7 +13,9 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int n = 0;
 
 	if (!b)
+		{
 		return (0);
+		}
 
 	for (x = 0; b[x] != '\0'; x++)
 	{
@@ -25,7 +27,9 @@ unsigned int binary_to_uint(const char *b)
 	{
 		n <<= 1;
 		if (b[x] == '1')
+			{
 			n += 1;
+			}
 	}
 
 	return (n);
