3675093ca0d947ef8b231e4acbddea15c36dcd35
JamesOKata <jamesokata3@gmail.com>
2023-05-04 14:22:14 +0300

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,10 +13,15 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int divisor, product;
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
+		{
 		return (-1);
+		}
 	divisor = 1 << index;
 	product = n & divisor;
 	if (product == divisor)
+		{
 		return (1);
+		}
+
 	return (0);
 }
